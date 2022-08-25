cc5b53a69e1907f78097489ca95716ef33ed83b7
John Fox <107318784+jff97@users.noreply.github.com>
2022-08-25 12:49:37 -0500

    commenting and stable refactoring

--- personalWebsite/javascript/stateEffects.js
+++ personalWebsite/javascript/stateEffects.js
@@ -1,4 +1,12 @@
+/**
+ * @file the jquery that makes the svg us map interactive 
+ * author John Fox wrote one function at the bottom
+ * author David Marcus wrote all other jquery functions
+ */
 
+/**
+ * when a state or the circle representing DC get hovered make a info box for the state appear next to the cursor
+ */
 $("path, circle").hover(
   function(e) {
     $('#info-box').css('display','block');
@@ -6,12 +14,18 @@ $("path, circle").hover(
   }
 );
 
+/**
+ * when a state or the circle representing DC have the cursor leave them hide the info box
+ */
 $("path, circle").mouseleave(
   function(e) {
     $('#info-box').css('display','none');
   }
 );
 
+/**
+ * Make the info box follow the cursor with a mouse move event
+ */
 $(document).mousemove(
   function(e) {
     $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
@@ -19,6 +33,9 @@ $(document).mousemove(
   }
 ).mouseover();
 
+/**
+ * Make the map work better on mobile
+ */
 var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 if(ios) {
   $('a').on('click touchend', function() {
@@ -29,7 +46,11 @@ if(ios) {
 }
 
 
-//my stuff below
+//Code written by John Fox below
+/**
+ * when a state or the circle representing DC gets clicked redirect the page to one of the state gallery pages with 
+ * a name that matches the id of the svg path or circle
+ */
 $("path, circle").click(
   function checkVisitedList(e) {
     for (var i = 0; i < visitedList.length; i++) {
