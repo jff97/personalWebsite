35104b74f9736e835db6d76b5e310fd438d532d6
John Fox <107318784+jff97@users.noreply.github.com>
2023-06-10 17:47:16 -0500

    seems to work

---
+++ personalWebsite/javascript/dualGalleryLoading.js
@@ -0,0 +1,36 @@
+function loadHighQualityImages() {
+   // Delay the image loading for 4 seconds
+   setTimeout(function () {
+     // Get all the images
+     var picFlowDivs = document.getElementsByClassName('picFlow');
+     var imageList = [];
+ 
+     for (var i = 0; i < picFlowDivs.length; i++) {
+       var images = picFlowDivs[i].getElementsByTagName('img');
+ 
+       for (var j = 0; j < images.length; j++) {
+         imageList.push(images[j]);
+       }
+     }
+     alert("Loading new")
+     // Start dual loading
+     for (var i = 0; i < imageList.length; i++) {
+       var image = imageList[i];
+       var lowQSrc = image.src;
+       var highQSrc = lowQSrc.replace('/statesLowQ/', '/statesHighQ/');
+       var highQImg = new Image();
+ 
+       highQImg.onload = (function (img, highQSrc) {
+         return function () {
+           img.src = highQSrc;
+         };
+       })(image, highQSrc);
+ 
+       highQImg.src = highQSrc;
+       image.parentNode.appendChild(highQImg);
+     }
+   }, 10000); // Delay for 1 second
+ }
+ 
+ window.addEventListener('load', loadHighQualityImages);
+ 
